89e128533eb45f9aca261c64654c59b7bc3eb6ec
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-09-22 08:16:52 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+
+
+const apiKey = '57df789ae4965f62a6a161f8b63d7642'
+
+const boston = '4930956'
+const newYorkCity = '5128581'
+
+
+let getWeather = async (city, cityName) => {
+    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=${city}&units=imperial&appid=${apiKey}`)
+    let data = await response.json();
+    document.querySelector('.currentWeather').innerHTML = data.main.temp;
+    document.querySelector('.cityName').innerHTML = cityName
+    return data.main.temp;
+}
+
+document.querySelector('.boston').onClick = getWeather(boston, 'Boston');
+// getWeather(boston, 'Boston');
+// getWeather(newYorkCity, 'New York');
+
+
+
+
+
+
+
+// async function callAsync() {
+//   let currentWeather = await getWeather();
+//   console.log(currentWeather);
+// }
+
+// callAsync();
+
+
+
+// let weather = async () => {
+//     let response = await fetch(`api.openweathermap.org/data/2.5/weather?id=${boston}&appid=${apiKey}`);
+//     let data = await response.json();
+//     return data;
+// }
+
+// weather().then(data => console.log(data));
+
+// fetch(`https://cat-fact.herokuapp.com/facts`)
+//     .then(response => {
+//         console.log(response.json());
+//     })
+//     .then(users => {
+//         console.log(users)
+//     });
+
+// fetch('https://jsonplaceholder.cypress.io/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+
+
+// function getWeather() {
+//   fetch(`http://api.openweathermap.org/data/2.5/weather?id=${boston}&units=imperial&appid=${apiKey}`)
+//   .then(function(res) {
+//       return res.json();
+//   })
+//   .then(function(data) {
+//       console.log(data.main)
+//       return data
+//   }).then(function(data){
+//     currentWeather = data.main.temp
+//   })
+//   .catch(error => console.log(error))
+// }
